a12db0d04fba6de1174bc522b672a6262fd3ffe3
Suzal3579 <pe.messh@gmail.com>
2017-06-07 09:55:23 +0545

    Socket.io installed and client scripts made available.

--- public/index.js
+++ public/index.js
@@ -1,7 +1,9 @@
 function setup() {
-
+    createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-
+    background(0);
+    fill(random(1, 255), random(1, 255), random(1, 255));
+    ellipse(mouseX, mouseY, 50, 50);
 }
--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Suzal is great.");
+console.log("Sever running at port 9999");
+var express = require("express");
+var app = express();
+var server = app.listen(9999);
+
+app.use(express.static("public"));
+
+var socket = require("socket.io");
+var ioTrack = socket(server);
+
+ioTrack.sockets.on("connection", (socket) => {
+    console.log(socket);
+});
