36892fd9d3498d7a7a92fdb3e11cd2857209105e
Suzal3579 <pe.messh@gmail.com>
2017-06-07 08:31:13 +0545

    Revising p5.js

---
+++ app.js
@@ -0,0 +1,1 @@
+console.log("Suzal is great.");
--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-var mic;
-var points = [];
-var amp;
-
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    mic = new p5.AudioIn();
-    mic.start();
+let setup = () => {
+    createCanvas(500, 500);
 }
 
-function draw() {
-    background(255, 255, 0);
-    var level = mic.getLevel();
-    points.push(level);
-    stroke(random(1, 255), random(1, 255), random(1, 255));
-    noFill();
-    beginShape();
-    for (var i = 0; i < points.length; i++) {
-        var y = map(points[i], 0, 1, height / 1.5, 0);
-        vertex(i, y);
-    }
-    endShape();
-    if (points.length > width) {
-        points.splice(0, 1);
-    }
+let draw = () => {
+    background(50);
+    ellipse(20, 20, 20, 20);
 }
